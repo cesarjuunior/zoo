23e6a1b3bd78ee5edc9bae2bccc3fd2722f5eafb
Rogério Feitoza <rogerio.feitoza@basis.com.br>
2019-07-03 20:46:36 -0300

    repository alimento

--- back/src/main/java/br/com/unb/zoo/repository/AlimentoRepository.java
+++ back/src/main/java/br/com/unb/zoo/repository/AlimentoRepository.java
@@ -3,10 +3,12 @@ package br.com.unb.zoo.repository;
 import br.com.unb.zoo.domain.Alimento;
 import br.com.unb.zoo.domain.model.ator.Ator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Repository
@@ -18,5 +20,12 @@ public interface AlimentoRepository extends JpaRepository<Alimento, Long> {
     @Query(value = "select distinct * from zoo.alimento as a WHERE a.id = :id", nativeQuery = true)
     Alimento buscarPorId(@Param("id") Long id);
 
+    @Modifying
+    @Transactional
+    @Query(value = "INSERT INTO zoo.alimento(id, nome, fornecedor) VALUES(:id, :nome, :idFornecedor) ", nativeQuery = true)
+    void salvar(@Param("id") Long id, @Param("idFornecedor") Long idFornecedor, @Param("nome") String nome);
 
+    @Modifying
+    @Query(value = "DELETE FROM zoo.animais WHERE id = :id;", nativeQuery = true)
+    void excluir(@Param("id") Long id);
 }
